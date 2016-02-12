9cc00aa9b237bda5230f325288c169bbb377b5d4
Alex Schedler <con.sume.org@gmail.com>
2016-02-12 17:55:14 +0100

    oauth implemented

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
+console.log = function(){
+	console.warn( JSON.stringify(arguments) );
+}
+
+// ###
 var express     = require('express');
 var bodyParser 	= require('body-parser');
-var OAuth 		= require('oauth');
+var session 	= require('cookie-session')
 var unirest		= require('unirest');
+var uuid 		= require('uuid');
+
 var app         = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( session({
+  	secret: process.env.COOKIE_SECRET,
+  	encrypted: true
+}) );
 
-app.set('port', (process.env.PORT || 3141));
+app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var config = {};
 app.set('CLOUDANT_KEY', 			process.env.CLOUDANT_KEY );
 app.set('CLOUDANT_PASSWORD',		process.env.CLOUDANT_PASSWORD );
 app.set('CLOUDANT_URL', 			process.env.CLOUDANT_URL );
@@ -21,6 +31,10 @@ app.set('FIDOR_OAUTH_CLIENT_SECRET',process.env.FIDOR_OAUTH_CLIENT_SECRET );
 app.set('PUSHOVER_API_KEY', 		process.env.PUSHOVER_API_KEY );
 app.set('SENDGRID_PASSWORD', 		process.env.SENDGRID_PASSWORD );
 app.set('SENDGRID_USERNAME', 		process.env.SENDGRID_USERNAME );
+app.set('FIDOR_AUTH_URL', 			process.env.FIDOR_AUTH_URL );
+app.set('FIDOR_API_URL', 			process.env.FIDOR_API_URL );
+
+app.set('STATE', 					Math.random() );
 
 // ##############
 
@@ -29,60 +43,77 @@ app.get('/', function(request, response) {
 });
 
 app.get('/redirect', function(request, response) {
-	var fidor_config = {
-		fidor_api_url  : "https://aps.fidor.de/",
-	  	fidor_oauth_url: "https://aps.fidor.de/oauth"
-	}
-    
-    var oauth2 = new OAuth.OAuth2(
-		app.get('FIDOR_OAUTH_CLIENT_ID'),
-      	app.get('FIDOR_OAUTH_CLIENT_SECRET'),
-      	fidor_config.fidor_api_url,
-      	'oauth/',
-      	'oauth/token',
-      	null
-    );
-    
-	console.log( oauth2 );
-	console.log( JSON.stringify(oauth2, null, "    ") );
-    
-    
-    oauth2.getOAuthAccessToken(
-    	'',
-      	{
-	      	'grant_type':'client_credentials'
-		},
-      	function(e, access_token, refresh_token, results){
-      		console.log('bearer: ',access_token);
-    	}
-    );
-    
-    
-    
-    
-    return
-    
-    
-    
-    var redirect_uri = encodeURIComponent( request.headers.host + "/oauth?ep=/account" )
-    var oauth_url = fidor_config.fidor_oauth_url +
-  				  	"/authorize?client_id=" + app.get('FIDOR_OAUTH_CLIENT_ID') +
-  				  	"&state=123&response_type=code&" +
-  				  	"redirect_uri=" + redirect_uri;
-  	console.log('OAuth Redirect to: ', oauth_url);
-    response.writeHead(307, {"location" : oauth_url})
-    response.end()
-    return
+
+    var oauth_url = app.get('FIDOR_AUTH_URL') + '/authorize' +
+    				'?client_id=' + app.get('FIDOR_OAUTH_CLIENT_ID') +
+    				'&state=' + app.get('STATE') +
+    				'&response_type=code' +
+    				'&redirect_uri=' + encodeURIComponent( 'http://' + request.headers.host + '/oauth?endpoint=/account' );
+    console.log('Redirect ->', oauth_url);
+    response.writeHead( 307, { 'location': oauth_url } );
+    response.end();
+        
 });
 
 app.get('/oauth', function(request, response) {
-	console.log( JSON.stringify(request, null, '    ') );
-	console.log('------------------------------')
-	console.log( JSON.stringify(response, null, '    ') );
+	var code	= request.query.code,
+		target	= request.query.endpoint;
+		
+	request.session.code = code;
+		
+	unirest.post( app.get('FIDOR_AUTH_URL') + '/token' )
+		.auth( app.get('FIDOR_OAUTH_CLIENT_ID'), app.get('FIDOR_OAUTH_CLIENT_SECRET'), true )
+		.send('code=' + code )
+		.send('client_id=' + app.get('FIDOR_OAUTH_CLIENT_ID') )
+		.send('redirect_uri=' + encodeURIComponent( "http://" + request.headers.host + "/oauth?endpoint=" + target) )
+		.send('grant_type=authorization_code')
+		.end( function(oauth_response){
+			console.log( 'TOKEN ->', oauth_response.body );
+			request.session.token = JSON.stringify( oauth_response.body );
+			response.writeHead( 307, { "location": target } );
+			response.end()
+		});
+		
 });
 
+
 app.get('/account', function(request, response) {
-	response.render('pages/index');
+	var token = JSON.parse(request.session.token);
+	var params = {
+		_id: "",
+		group: (Math.random() * 5 | 0) + 1,
+		token: token,
+		user: null,
+		account: null,
+		notification: {
+			pushover: "",
+			email: ""
+		}
+	}
+	
+	unirest.get( app.get('FIDOR_API_URL') + 'accounts' )
+		.header( 'Authorization', 'Bearer ' + token.access_token)
+		.header( 'Accept', 'application/vnd.fidor.de; version=1,text/json')
+		.end( function(r){
+			var result = r.body[0];
+			params._id = result.id + "-" + uuid.v4();
+			params.user = {
+				name: result.customers[0].first_name,
+				nick: result.customers[0].nick,
+				last_update: result.customers[0].updated_at
+			};
+			params.notification = {
+				email: result.customers[0].email,
+				pushover: ""
+			};
+			params.account = {
+				id: result.id,
+				iban: result.iban,
+				balance: result.balance,
+				last_update: result.updated_at
+			}
+			response.render('account', params);
+		} );
 });
 
 // ##############
