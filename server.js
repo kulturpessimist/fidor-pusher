c947093d1db5f78f9d1e09b3b335381589db0571
Alex Schedler <con.sume.org@gmail.com>
2016-02-18 18:51:39 +0100

    small progress

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var express     = require('express');
 var bodyParser 	= require('body-parser');
 var session 	= require('cookie-session')
 var unirest		= require('unirest');
-var uuid 		= require('uuid');
 
 var app         = express();
 
@@ -37,27 +36,75 @@ app.set('FIDOR_API_URL', 			process.env.FIDOR_API_URL );
 app.set('STATE', 					Math.random() );
 
 // ##############
+function checkSessionToken(request, response){
+	if( request.session.hasOwnProperty('token') ){
+		console.log('Token', request.session.token);
+		if( request.session.token.length > 0 ){ // token was set sometime in the past
+			var token = JSON.parse(request.session.token),
+				now = Math.round( Date.now() / 1000 ),
+				expires = (token.issued + token.expires_in)
+						
+			if( now > expires ){ //check if token is expired
+				// refresh token
+				console.log('Expired token found...');
+				response.writeHead( 307, { "location": '/refresh' } );
+				response.end();
+				return;
+			}
+			// allright return token as object
+			console.log('Perfect token found...');
+			return token;
+		}
+
+		console.log('Empty token found...');
+	}
+
+	// no token? send user to redirect to login with fidor...
+	console.log('No token found...');
+	request.session.token = "";
+	response.writeHead( 307, { 'location': '/redirect' } );
+	response.end();
+	return;
+
+}
+
+function saveAccountInCouch(doc, callback){
+	
+	unirest.head( app.get('CLOUDANT_URL') + '/' + doc._id )
+		.auth( app.get('CLOUDANT_KEY'), app.get('CLOUDANT_PASSWORD'), true )
+		.send()
+		.end(function(couch_response){
+			if(couch_response.statusCode == 200){
+				// update the doc with the proper _rev
+				doc._rev = JSON.parse(couch_response.headers.etag);
+			}
+			// otherwise just create it...
+			unirest.put( app.get('CLOUDANT_URL') + '/' + doc._id )
+				.auth( app.get('CLOUDANT_KEY'), app.get('CLOUDANT_PASSWORD'), true )
+				.type('json')
+				.send(doc)
+				.end( function(couch_response){
+					console.log( 'PUT', couch_response );
+				});
+		});
+
+}
+// ##############
 
 app.get('/', function(request, response) {
 	response.render('index');
 });
 
 app.get('/redirect', function(request, response) {
-	var token = JSON.parse(request.session.token);
 
-	if( token.hasOwnProperty('refresh_token') ){
-		response.writeHead( 307, { "location": '/refresh' } );
-		response.end();
-	}else{
-		var oauth_url = app.get('FIDOR_AUTH_URL') + '/authorize' +
-						'?client_id=' + app.get('FIDOR_OAUTH_CLIENT_ID') +
-						'&state=' + app.get('STATE') +
-						'&response_type=code' +
-						'&redirect_uri=' + encodeURIComponent( 'http://' + request.headers.host + '/oauth?endpoint=/account' );
-		console.log('Redirect ->', oauth_url);
-		response.writeHead( 307, { 'location': oauth_url } );
-		response.end();
-	}
+	var oauth_url = app.get('FIDOR_AUTH_URL') + '/authorize' +
+					'?client_id=' + app.get('FIDOR_OAUTH_CLIENT_ID') +
+					'&state=' + app.get('STATE') +
+					'&response_type=code' +
+					'&redirect_uri=' + encodeURIComponent( 'http://' + request.headers.host + '/oauth?endpoint=/account' );
+	console.log('Redirect ->', oauth_url);
+	response.writeHead( 307, { 'location': oauth_url } );
+	response.end();
 
 });
 
@@ -98,23 +145,20 @@ app.get('/refresh', function(request, response) {
 			oauth_response.body.issued = Math.round(Date.now() / 1000);
 			request.session.token = JSON.stringify( oauth_response.body );
 			response.writeHead( 307, { 'location': '/account' } );
-			response.end()
+			response.end();
 		});
 
 });
 
 app.get('/account', function(request, response) {
-	var token = JSON.parse(request.session.token);
+	var token = checkSessionToken(request, response); //JSON.parse(request.session.token);
 	var params = {
 		_id: "",
-		group: (Math.random() * 5 | 0) + 1,
+		//group: (Math.random() * 5 | 0) + 1,
 		token: token,
 		user: null,
 		account: null,
-		notification: {
-			pushover: "",
-			email: ""
-		}
+		notification: null
 	}
 
 	unirest.get( app.get('FIDOR_API_URL') + 'accounts' )
@@ -127,15 +171,21 @@ app.get('/account', function(request, response) {
 				response.end();
 			}else{
 				var result = r.body[0];
-				params._id = result.id + "-" + uuid.v4();
+				params._id = result.id;
 				params.user = {
 					name: result.customers[0].first_name,
 					nick: result.customers[0].nick,
 					last_update: result.customers[0].updated_at
 				};
 				params.notification = {
-					email: result.customers[0].email,
-					pushover: ""
+					email: {
+						address: result.customers[0].email,
+						active: false
+					},
+					pushover: {
+						key: "",
+						active: false
+					}
 				};
 				params.account = {
 					id: result.id,
@@ -143,9 +193,20 @@ app.get('/account', function(request, response) {
 					balance: result.balance,
 					last_update: result.updated_at
 				}
+				
+				saveAccountInCouch(params);
+				
 				response.render('account', { params: params });
 			}
 		} );
+});
+
+app.get('/delete', function(request, response) {
+
+	request.session.token = "";
+	response.writeHead( 307, { 'location': '/' } );
+	response.end();
+
 });
 
 // ##############
